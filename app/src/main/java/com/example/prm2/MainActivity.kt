98174b0586f357699d9fd7d36fbbae724f7f4a89
mateczeleko <mateczeleko@icloud.com>
2024-06-18 20:51:48 +0200

    komit

---
+++ app/src/main/java/com/example/prm2/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.example.prm2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.findNavController
+import com.example.prm2.ui.theme.PRM2Theme
+
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            PRM2Theme {
+                val navController = rememberNavController()
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .navigationBarsPadding(), topBar = {
+                    TopAppBar(title = { Button(onClick = { navController.navigate(route = "home") }, content = { Text("Diary")})
+
+                    })
+                }) { innerPadding ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+
+                        NavHost(navController = navController, startDestination = "home")  {
+                            composable("home") {
+                                HomeScreen(navController = navController, diaryEntries = listOf("Entry 1", "Entry 2", "Entry 3", "Entry 4",
+                                    "Entry 5", "Entry 6", "Entry 7", "Entry 8", "Entry 9", "Entry 10", "Entry 11", "Entry 12", "Entry 13", "Entry 14", "Entry 15"))
+                            }
+                            composable("addEntry") {
+                                DiaryEntryScreen()
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiaryEntryScreen() {
+    var title by remember { mutableStateOf(TextFieldValue()) }
+    var content by remember { mutableStateOf(TextFieldValue()) }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "New Diary Entry",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        TextField(
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Title") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = content,
+            onValueChange = { content = it },
+            label = { Text("Content") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
+            Text("Save")
+
+        }
+
+    }
+}
+@Composable
+fun HomeScreen(navController: NavController, diaryEntries: List<String>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            items(diaryEntries) { entry ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = entry,
+                        modifier = Modifier.padding(16.dp)
+                    )
+
+                }
+            }
+        }
+        FloatingActionButton(
+            onClick = { navController.navigate("addEntry") },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = stringResource(id = R.string.add_entry))
+
+        }
+    }
+}
